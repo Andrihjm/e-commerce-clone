ec2df298e8c352a10884563c834289519d004c5c
andri <andri@pop-os.localdomain>
2024-08-14 11:34:39 +0700

    create header

---
+++ components/shared/header.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import LogoPizza from "../../app/assets/images/pizza.png";
+import { Button } from "../ui/button";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="border-custom-primary">
+        <div className="components flex items-center justify-between py-2">
+          <div className="flex items-center gap-4">
+            <Image
+              src={LogoPizza}
+              alt="Logo"
+              width={60}
+              height={60}
+              className="object-cover"
+            />
+
+            <div className="">
+              <h1 className="text-2xl font-black uppercase">Next Pizza</h1>
+              <p className="leading-3 text-sm text-gray-50">Beli yay woyyyy</p>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Button variant={"outline"} className="flex items-center gap-1">
+              <User size={16} />
+              User
+            </Button>
+
+            <div>
+              <Button className="group relative">
+                <p>$520</p>
+                <span className="h-full w-[1px] mx-3 bg-white/30" />
+
+                <div className="flex items-center gap-1 transition-all duration-300 group-hover:opacity-0">
+                  <ShoppingCart size={16} strokeWidth={2} />
+                  <p>3</p>
+                </div>
+
+                <ArrowRight
+                  size={16}
+                  className="absolute right-5 transition-all duration-500 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+                />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
