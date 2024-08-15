097cab1ab53be74235e3c02ff89b9fe94c921039
andri <andri@pop-os.localdomain>
2024-08-15 07:34:00 +0700

    benerin ui

--- components/shared/range-slider.tsx
+++ components/shared/range-slider.tsx
@@ -69,11 +69,11 @@ const RangeSlider = React.forwardRef(
                 top: `10px`,
               }}
             >
-              <span className="text-sm">
+              <span className="absolute -right-1.5 text-sm">
                 {formatLabel ? formatLabel(value) : value}
               </span>
             </div>
-            <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-primary/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+            <SliderPrimitive.Thumb className="cursor-pointer block h-4 w-4 rounded-full border border-primary/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
           </React.Fragment>
         ))}
       </SliderPrimitive.Root>
