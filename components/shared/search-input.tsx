429c9c757cb09b7e263cff6cf4e2ad8780607ac1
andri <andri@pop-os.localdomain>
2024-08-17 13:40:13 +0700

    create search products

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -1,21 +1,33 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Flashlight, Search } from "lucide-react";
-import { useRef, useState } from "react";
-import { useClickAway } from "react-use";
+import { ApiClient } from "@/services/api-client";
+import { Product } from "@prisma/client";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { useClickAway, useDebounce } from "react-use";
 
 const SearchInput = () => {
   const [isFocused, setIsFocused] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
   const ref = useRef(null);
-
-  //   const handleClickAwayUse = useClickAway(ref, () => {
-  //     setIsFocused(!IsFocused);
-  //   });
 
   useClickAway(ref, () => {
     setIsFocused(false);
   });
+
+  useDebounce(
+    () => {
+      ApiClient.products.search(searchQuery).then((items) => {
+        setProducts(items);
+      });
+    },
+    250,
+    [searchQuery]
+  );
 
   return (
     <>
@@ -31,20 +43,39 @@ const SearchInput = () => {
       >
         <Search className="absolute top-1/2 translate-y-[-50%] left-3 h-5 text-gray-400" />
         <input
-          className="rounded-2xl outline-none w-full bg-gray-100 pl-11"
+          className="rounded-2xl outline-none w-full text-black bg-gray-100 pl-11"
           type="text"
           placeholder="Найти пиццу..."
           onFocus={() => setIsFocused(true)}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
 
-        <div
-          className={cn(
-            "w-full absolute top-14 py-2 shadow-md rounded-xl bg-white transition-all duration-200 invisible opacity-0 z-30",
-            isFocused && "visible font-semibold text-black px-4 opacity-100 top-12"
-          )}
-        >
-          Haris Andri
-        </div>
+        {products.length > 0 && (
+          <div
+            className={cn(
+              "w-full absolute top-14 py-2 shadow-md rounded-xl bg-white transition-all duration-200 invisible opacity-0 z-30",
+              isFocused && "visible font-semibold text-black opacity-100 top-12"
+            )}
+          >
+            {products.map((product) => (
+              <Link
+                key={product.id}
+                href={`/product/1`}
+                className="w-full flex items-center px-4 py-2 gap-2 hover:bg-primary/30"
+              >
+                <Image
+                  src={product.imageUrl}
+                  alt={product.name}
+                  width={25}
+                  height={25}
+                  className="object-contain rounded-full"
+                />
+                <span>{product.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
