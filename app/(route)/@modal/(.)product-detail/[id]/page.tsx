f41b4c6061705bcb81e4150ab6426329ca71ed83
andri <andri@pop-os.localdomain>
2024-08-19 12:52:43 +0700

    setUp parallel routes

---
+++ app/(route)/@modal/(.)product-detail/[id]/page.tsx
@@ -0,0 +1,52 @@
+import PizzaImage from "@/components/products/pizza-image";
+import prisma from "@/lib/db";
+import React from "react";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const page = async ({ params }: Props) => {
+  const productApi = await prisma.product.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+
+  console.log(productApi);
+
+  if (!productApi) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div className="components flex flex-col my-10">
+      <div className="flex flex-1">
+        <PizzaImage imageUrl={productApi.imageUrl} size={40} />
+
+        <div className="w-[490px] bg-gray-500/10 p-7">
+          <h2
+            title={productApi.name}
+            className="mb-1 line-clamp-2 font-extrabold"
+          >
+            {productApi.name} Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Optio dicta earum incidunt eum id expedita,
+            reprehenderit necessitatibus vero minima autem?
+          </h2>
+          <p
+            title={productApi.description}
+            className="line-clamp-2 text-gray-400"
+          >
+            {productApi.description} Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Quis nisi aperiam dolores laborum? Est, adipisci
+            repellendus quidem aspernatur quam facere?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,10 +1,17 @@
 import Header from "@/components/shared/header";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = ({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) => {
   return (
     <main className="min-h-screen">
       <Header />
       {children}
+      {modal}
     </main>
   );
 };
